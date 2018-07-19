04908100b0d2a111f8fa6855b77c4d9eec062731
Vasilis Spilka <vasspilka@gmail.com>
2018-07-19 12:17:34 +0200

    add basic formater

---
+++ .formatter.exs
@@ -0,0 +1,14 @@
+  [
+    inputs: [
+      "mix.exs",
+      "lib/**/*.{ex,exs}",
+      "dev/**/*.{ex,exs}",
+      "test/**/*.{ex,exs}",
+      "priv/**/*.{ex,exs}"
+    ],
+    line_length: 85,
+    locals_without_parens: [
+      plug: 1,
+      plug: 2
+    ]
+  ]
